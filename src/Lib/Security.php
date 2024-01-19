4fc4c8766280fab082d168eb7ef54a4ee726517b
raulgodii <raulgodii13@gmail.com>
2024-01-19 14:23:24 +0100

    getToken

--- src/Lib/Security.php
+++ src/Lib/Security.php
@@ -1,25 +1,30 @@
 <?php
 
 namespace Lib;
+
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 use PDOException;
 
 class Security
 {
-    final public static function claveSecreta(){
+    final public static function claveSecreta()
+    {
         return $_ENV['SECRET_KEY'];
     }
-    final public static function encriptaPassw(string $pass){
+    final public static function encriptaPassw(string $pass)
+    {
         $pass = password_hash($pass, PASSWORD_DEFAULT);
         return $pass;
     }
 
-    final public static function validaPassw(string $pass, string $passHash){
+    final public static function validaPassw(string $pass, string $passHash)
+    {
         return password_verify($pass, $passHash);
     }
 
-    final public static function crearToken(string $key, array $data):string{
+    final public static function crearToken(string $key, array $data): string
+    {
         $time = strtotime("now");
         $token = array(
             "int" => $time,
@@ -29,17 +34,18 @@ class Security
         return JWT::encode($token, $key, "HS256");
     }
 
-    final public static function getToken(){
-        $headers = apache_request_headers();
-        if(!isset($headers['Authorization'])){
+    final public static function getToken()
+    {
+        $headers = apache_request_headers(); // Recoger las cabeceras en el servidor Apache
+        if (!isset($headers['Authorization'])) { // Comprobamos que existe la cabecera authorization
             return $response['message'] = json_decode(ResponseHTTP::statusMessage(403, 'Acceso Denegado'));
         }
-        try{
+        try {
             $authorizationArr = explode('', $headers['Authorization']);
             $token = $authorizationArr[1];
             $decodeToken = JWT::decode($token, new Key(Security::claveSecreta(), 'HS256'));
             return $decodeToken;
-        } catch(PDOException $e){
+        } catch (PDOException $e) {
             return $response['message'] = json_encode(ResponseHTTP::statusMessage(401, 'Token expirado o invalido'));
         }
     }
